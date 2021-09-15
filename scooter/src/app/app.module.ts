7fea1d66aa4fd55e72d4a9edc9ff432edd91ea9b
Po Man <pman@prodessebv.nl>
2021-09-15 12:54:03 +0200

    commit the components welcome and header

--- scooter/src/app/app.module.ts
+++ scooter/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
