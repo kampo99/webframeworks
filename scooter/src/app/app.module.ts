8e907302e517e47e4f8c177e8f887b9bfbf2235e
Po Man <pman@prodessebv.nl>
2021-09-16 11:08:42 +0200

    navbar inplemented

--- scooter/src/app/app.module.ts
+++ scooter/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
