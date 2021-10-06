d8a01b0fc7e636eccc74535a65d95c9a0082bba6
Po Man <po.man@hva.nl>
2021-10-06 23:05:25 +0200

    unknown route component made

--- scooter/src/app/app-routing.module.ts
+++ scooter/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import {WelcomeComponent} from "./welcome/welcome.component";
 import {Overview31Component} from "./scooters/overview31/overview31.component";
 import {Overview32Component} from "./scooters/overview32/overview32.component";
 import {Overview33Component} from "./scooters/overview33/overview33.component";
+import {UnknownRouteComponent} from "./unknown-route/unknown-route.component";
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
+  {path: 'home', component: WelcomeComponent},
   {path: 'scooters/overview31', component: Overview31Component},
   {path: 'scooters/overview32', component: Overview32Component},
   {path: 'scooters/overview33', component: Overview33Component},
+  {path: 'sign-up', component: UnknownRouteComponent},
+  {path: 'sign-in', component: UnknownRouteComponent}
 ];
 
 @NgModule({
