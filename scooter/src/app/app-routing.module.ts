9de8518b6aec228e199dbf5ab4fa6c94c8999a91
Wesley Thomas <wesley.thomas@hva.nl>
2021-10-20 16:35:59 +0200

    Changes by Po

--- scooter/src/app/app-routing.module.ts
+++ scooter/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import {Overview31Component} from "./scooters/overview31/overview31.component";
 import {Overview32Component} from "./scooters/overview32/overview32.component";
 import {Overview33Component} from "./scooters/overview33/overview33.component";
 import {UnknownRouteComponent} from "./unknown-route/unknown-route.component";
-import {Detail33Component} from "./scooters/detail33/detail33.component";
+import {Detail34Component} from "./scooters/detail34/detail34.component";
+import {Overview34Component} from "./scooters/overview34/overview34.component";
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
@@ -13,10 +14,10 @@ const routes: Routes = [
   {path: 'scooters/overview31', component: Overview31Component},
   {path: 'scooters/overview32', component: Overview32Component},
   {path: 'scooters/overview33', component: Overview33Component},
-  {path: 'scooters/overview34', component:Overview33Component,
+  {path: 'scooters/overview34', component:Overview34Component,
     children: [
       {path: '', pathMatch: 'full', redirectTo: 'O'},
-      {path:':id', component: Detail33Component}
+      {path:':id', component: Detail34Component}
     ]},
   {path: 'sign-up', component: UnknownRouteComponent},
   {path: 'sign-in', component: UnknownRouteComponent},
